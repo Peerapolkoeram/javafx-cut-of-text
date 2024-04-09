9c9536c0dc4672904ae135d0e05b6e32f06ad1ca
taknaja <peerapolkoeram@gmail.com>
2024-04-09 23:51:58 +0700

    update service get sheetname

--- src/main/java/com/tpk/javafxcutoftext/service/FileServiceImp.java
+++ src/main/java/com/tpk/javafxcutoftext/service/FileServiceImp.java
@@ -1,27 +1,36 @@
 package com.tpk.javafxcutoftext.service;
 
 import com.tpk.javafxcutoftext.model.SheetExcelModel;
+import com.tpk.javafxcutoftext.utils.FilesUtils;
+import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 public class FileServiceImp {
+
+    private final FilesUtils filesUtils;
 
     public List<SheetExcelModel> getSheetExcel(Workbook sheets) {
         List<SheetExcelModel> listSheetName = new ArrayList<>();
-        sheets.forEach(sheet -> {
-            int numberSheet = sheets.getNumberOfSheets();
-            String sheetName = sheets.getSheetName(numberSheet);
+        for (int i = 0; i < sheets.getNumberOfSheets(); i++) {
+            int sheetNumber = sheets.getNumberOfSheets();
+            String sheetName = sheets.getSheetName(i);
             listSheetName.add(SheetExcelModel.builder()
                     .sheetName(sheetName)
-                    .value(numberSheet)
+                    .sheetNumber(sheetNumber)
                     .build());
-        });
+        }
+
         return listSheetName;
     }
 
-    
-
+   public List<String> getDataInExcel(String pathFile) {
+        FileInputStream fileInputStream = filesUtils.fileInputStream(pathFile);
+        return null;
+   }
 
 }
--- src/main/java/com/tpk/javafxcutoftext/utils/FilesUtils.java
+++ src/main/java/com/tpk/javafxcutoftext/utils/FilesUtils.java
@@ -1,6 +1,8 @@
 package com.tpk.javafxcutoftext.utils;
 
 import lombok.SneakyThrows;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 
@@ -9,6 +11,11 @@ public class FilesUtils {
     @SneakyThrows
     public FileInputStream fileInputStream(String pathFile) {
         return new FileInputStream(pathFile);
+    }
+
+    @SneakyThrows
+    public Workbook workbook(FileInputStream file) {
+        return new XSSFWorkbook(file);
     }
 
 
